94294ba23ea4fe3ff66950a9317754e9012e6631
dejan-simonovski <dejan.simonovski2002@gmail.com>
2024-02-20 19:09:30 +0100

    Fix to Home and Add Memory Game

---
+++ src/views/MemoryView.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      <Memory/>
+    </div>
+  </template>
+  
+  <script>
+  import Memory from '@/components/MemoryGame.vue'
+  
+  export default {
+    name: 'MemoryView',
+    components: {
+        Memory
+    }
+  }
+  </script>
+  
